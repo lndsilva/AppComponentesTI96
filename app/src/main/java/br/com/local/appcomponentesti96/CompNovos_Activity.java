b2d8dffb62654728b994f35404d5ecad50e25420
Laercio-Note <lndsilva@hotmail.com>
2021-03-25 15:42:39 -0300

    Initial commit

---
+++ app/src/main/java/br/com/local/appcomponentesti96/CompNovos_Activity.java
@@ -0,0 +1,14 @@
+package br.com.local.appcomponentesti96;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CompNovos_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.comp_novos_layout);
+    }
+}
---
+++ app/src/main/java/br/com/local/appcomponentesti96/OutrosComponentes_Activity.java
@@ -0,0 +1,14 @@
+package br.com.local.appcomponentesti96;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OutrosComponentes_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.outros_componentes_layout);
+    }
+}
